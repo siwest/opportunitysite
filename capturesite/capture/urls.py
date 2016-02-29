cb61efbaa80855a6bc4617be4cd38b36be6123aa
Sarah West <sarah.irene.west@gmail.com>
2016-02-29 15:34:08 -0800

    Corrects paths

--- capturesite/capture/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from capture import views
-
-
-urlpatterns = [ url(r'^$', views.index, name='index'), 
-		url(r'^(?P<opportunity_id>\d+)/$', views.oppdetail, name = 'oppdetail'),
-		url(r'^(?P<opportunity_id>\d+)/results', views.oppresults, name='oppresults'),
-	]
